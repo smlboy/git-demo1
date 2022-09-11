b3eb89f89873d201cc191f78468be4056db9e672
qwl <1852573024@qq.com>
2022-09-11 11:11:45 +0800

    second commit

--- src/main/java/com/smlboy/git/GitTest.java
+++ src/main/java/com/smlboy/git/GitTest.java
@@ -3,5 +3,6 @@ package com.smlboy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("idea git add cache!");
+        System.out.println("idea git add 2!");
     }
 }
